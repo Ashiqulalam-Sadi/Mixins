6d42e213badef8c0c1799671c759a8c9e591be86
Ashiqulalam-Sadi <72812072+Ashiqulalam-Sadi@users.noreply.github.com>
2022-08-08 20:31:56 +0600

    Create mixins.dart

---
+++ mixins.dart
@@ -0,0 +1,30 @@
+mixin Sugar{
+  void checkSugar(){
+    print("Add Sugar if not added yet");
+  }
+}
+
+class Tea with Sugar{
+  making(){
+    print("Tea is making");
+  }
+}
+
+class MilkShake with Sugar{
+  mangoShake(){
+    print("mangoShake is making");
+  }
+}
+
+void main(){
+  Tea t= Tea();
+  t.making();
+  t.checkSugar();
+
+  print("\n");
+
+  MilkShake m=MilkShake();
+  m.mangoShake();
+  m.checkSugar();
+  
+}
